c33dfd4817eca3cc7070c3728f996c178c3326d8
Ankit Sharma <ankitsharma290101@gmail.com>
2023-10-04 17:23:49 +0530

    Setup Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+//Pages
+import Home from "./pages/Home"
+import PricingPage from "./pages/PricingPage";
+import SignupPage from "./pages/SignupPage";
+import GlobalStyle from "./globalStyles";
+
 function App() {
+  const appRouter = createBrowserRouter([
+    {
+      path:"/",
+      element:<Home/>
+    },
+    {
+      path:"pricing",
+      element:<PricingPage/>
+    },
+    {
+      path:"signup",
+      element:<SignupPage/>
+    },
+  ])
   return (
-    <div className="App">
-      App
-    </div>
+    <RouterProvider router={appRouter}>
+     <GlobalStyle/>
+  
+    </RouterProvider>
   );
 }
 
